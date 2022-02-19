768aed88cb2bebb20db28a87b247c8ac9bf1333a
Loppes Filho <loppes@megganet.com.br>
2022-02-19 13:45:39 -0300

    h2, lombok, jpa and Category

---
+++ backend/src/main/java/com/udemy/mc/domain/controller/CategoryControlle.java
@@ -0,0 +1,16 @@
+package com.udemy.mc.domain.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/categories")
+public class CategoryControlle {	
+	
+	@GetMapping
+	public String list() {
+		return "Teste";
+	}			
+
+}
---
+++ backend/src/main/java/com/udemy/mc/domain/entity/Category.java
@@ -0,0 +1,33 @@
+package com.udemy.mc.domain.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+@Table(name = "category")
+public class Category implements Serializable{
+	private static final long serialVersionUID = 1L;
+
+	@EqualsAndHashCode.Include
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String name;
+}
