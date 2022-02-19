be86a4932e4d6f26db4631a9c30a61680f9f633f
Loppes Filho <loppes@megganet.com.br>
2022-02-19 11:29:49 -0300

    Project created

---
+++ backend/src/main/java/com/udemy/mc/UdemyMcApplication.java
@@ -0,0 +1,13 @@
+package com.udemy.mc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdemyMcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdemyMcApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/udemy/mc/UdemyMcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udemy.mc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdemyMcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
